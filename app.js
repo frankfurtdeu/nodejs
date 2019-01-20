53df116c6b32fe8fbb5e756613380bc63f466a9f
frankfurtdeu <46764030+frankfurtdeu@users.noreply.github.com>
2019-01-21 00:22:46 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get("/", function(req, res){
+    console.log("SOMEONE REQUESTED US!");
+});
+
+ app.listen(8080, function(){
+   console.log('App listening on port 8080!');
+ });
