6d378ab3844d38ac895cd1694a502ea5787590c3
frankfurtdeu <46764030+frankfurtdeu@users.noreply.github.com>
2019-01-21 00:48:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get("/", function(req, res){
-    console.log("SOMEONE REQUESTED US!");
+    res.send("You've reached the homepage!");
 });
 
  app.listen(443, function(){
